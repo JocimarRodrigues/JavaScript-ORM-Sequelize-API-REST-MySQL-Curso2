2b31931d7700121563759cb05d7dad4adae77a15
JocimarRodrigues <jocimarrodrigues1337@gmail.com>
2023-06-23 10:38:50 -0300

    Iniciando aula 05

---
+++ Aula-05-Servicos-Refatorando-Codigo/api/routes/pessoasRoute.js
@@ -0,0 +1,23 @@
+const { Router } = require('express')
+const PessoaController = require('../controllers/PessoaController')
+
+const router = Router()
+
+router
+  .get('/pessoas', PessoaController.pegaPessoasAtivas)
+  .get('/pessoas/todos', PessoaController.pegaTodasAsPessoas)
+  .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+  .get('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.pegaUmaMatricula)
+  .get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
+  .get('/pessoas/matricula/:turmaId/confirmados', PessoaController.pegaMatriculasPorTurma)
+  .get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas)
+  .post('/pessoas', PessoaController.criaPessoa)
+  .put('/pessoas/:id', PessoaController.atualizaPessoa)
+  .delete('/pessoas/:id', PessoaController.apagaPessoa)
+  .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+  .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
+  .post('/pessoas/:estudanteId/cancela', PessoaController.cancelaPessoa)
+  .put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
+  .delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
+
+module.exports = router
